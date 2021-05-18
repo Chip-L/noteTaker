5c5d23307d0c77e34096aa4024a74b6f85a2e525
Chip Long <chip_long@yahoo.com>
2021-05-18 15:52:18 -0600

    basic page navigation set up. added spacing to their html files for readability

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 // Express Server for NoteTaker
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 8080;
+
+//DATA - ignore for now
+
+// ROUTES
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "db", "db.json"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+
+app.get("*", (req, res) => {
+  const url = req.url === "/" ? "/index.html" : req.url;
+  res.sendFile(path.join(__dirname, `/public${url}`));
+});
+
+// LISTENER
+app.listen(PORT, () => {
+  console.log(`Note Taker App listening on PORT ${PORT}`);
+});
